e1b56a705410f8626c5f43313e7257c62b8d52ef
Simone Cocco <coccosimone@proton.me>
2025-03-22 20:31:46 +0100

    implementazione api

--- controllers/rest_api.py
+++ controllers/rest_api.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from subprocess import run as run_cmd
+from controllers.flux_control import IN_QUEUE
 
 app = Flask(__name__)
 
@@ -16,6 +18,51 @@ def get_samples():
         packets = list(main_shared_dict['packet_array'])
 
     return jsonify({'packets': packets})
+
+@app.route('/service', methods=['POST'])
+def add_service():
+    port = int(request.json['port'])
+    alias = request.json.get('alias', f'service_{port}')
+    for direction in ['OUTPUT', 'INPUT']:
+        for comm_port in ['s', 'd']:
+            for proto in ['tcp', 'udp']:
+                run_cmd([
+                    'sudo', 'iptables',
+                    '-A', direction,
+                    '-p', proto,
+                    f'--{comm_port}port', str(port),
+                    '-j', 'NFQUEUE', '--queue-num', str(IN_QUEUE)
+                ])
+    with main_process_lock:
+        main_shared_dict['services'] = main_shared_dict['services'][port] = alias
+
+    return f'services at {port} ({alias}) added correctly'
+
+@app.route('/service', methods=['GET'])
+def get_services():
+    with main_process_lock:
+        services = {
+            service_port:main_shared_dict['services'][service_port]
+            for service_port in main_shared_dict['services'].keys()
+        }
+
+    return jsonify(services)
+
+@app.route('/service', methods=['DELETE'])
+def delete_service():
+    pass
+
+@app.route('/rule', methods=['POST'])
+def create_rule():
+    pass
+
+@app.route('/rule', methods=['GET'])
+def get_rules():
+    pass
+
+@app.route('/rule', methods=['DELETE'])
+def delete_rule():
+    pass
 
 def start_rest_api(flask_port, process_lock, logger, shared_dict):
     global main_process_lock
