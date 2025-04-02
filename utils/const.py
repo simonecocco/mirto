d94bc484fb275402548e696633f4b23ac11bb2f9
Simone Cocco <coccosimone@proton.me>
2025-04-02 10:52:14 +0200

    aggiunta costante per dict

--- utils/const.py
+++ utils/const.py
@@ -4,6 +4,7 @@ PACKET_ARRAY_KEY = 'packet_array'
 SERVICES_KEY = 'services'
 FW_NOT_APPLICABLE_PORTS = [22]
 FW_RULES_LIST = 'firewall-rules'
+FW_RULES_HASH_SET = 'fw-rules-hashes'
 
 def get_queue_num(shared_dict):
     return shared_dict.get(QUEUE_NUM_KEY, DEFAULT_QUEUE_NUM)
