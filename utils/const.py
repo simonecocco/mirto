d0d34e81e4cd172a2c525e6e6a3b77658bca22cc
Simone Cocco <coccosimone@proton.me>
2025-04-07 13:49:49 +0200

    modifiche lievi

--- utils/const.py
+++ utils/const.py
@@ -5,6 +5,3 @@ SERVICES_KEY = 'services'
 FW_NOT_APPLICABLE_PORTS = [22]
 FW_RULES_LIST = 'firewall-rules'
 FW_RULES_HASH_SET = 'fw-rules-hashes'
-
-def get_queue_num(shared_dict):
-    return shared_dict.get(QUEUE_NUM_KEY, DEFAULT_QUEUE_NUM)
