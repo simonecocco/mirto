45ddf5f5b5755310a4fa5bf6aea699ccec48e2d9
Simone Cocco <coccosimone@proton.me>
2025-04-08 13:59:37 +0200

    migliorati

--- controllers/firewall/firewall_logic.py
+++ controllers/firewall/firewall_logic.py
@@ -1,91 +1,93 @@
-from controllers.firewall.rule import Rule
+from typing import Dict, Set
+from threading import Lock
+from collections import defaultdict
+from controllers.firewall.rule import *
+import logging
 from utils.const import *
-from numba import njit
-from typing import Dict, List, Set
-
-'''
-PACKET
-.drop()
-.accept(mangle:int=0) -> MANGLE_MARK o MANGLE_PAYLOAD
-.mangle()
-.verdict(action:int, mangle:int=0) -> DROP, ACCEPT, REPEAT, STOP
-Raises NoSuchAttributeException
-.cap_len -> payload_len
-.gid -> packet uid
-.hw_protocol -> HW protocol id
-.hook -> netfilter hook id
-.time -> packet arrival time
-.truncated ->true if packet payload is truncated
-.uid -> packet uid
-.mark -> get and set, unsigned integer 32bit
-.payload -> set value needs to be byte
-'''
 
 class FuocoDiMuro:
-    def __init__(self, process_lock, logger, shared_dict, rules=[]):
-        assert rules is not None, 'rule can\'t be none!'
-
+    def __init__(self, process_lock, logger, shared_dict, rules=None):
         self.main_process_lock = process_lock
         self.main_logger = logger
         self.main_shared_dict = shared_dict
-
-        self.rules = {}
-        self.fw_str_rules_copy: Set[str] = {rule_str for rule_str in self.main_shared_dict[FW_RULES_HASH_SET].values()}
-        for rule in self.main_shared_dict[FW_RULES_HASH_SET].values():
-            rule_obj: Rule = Rule(rule)
-            self.rules[rule_obj.get_hash()] = rule_obj
-            self.main_logger.info(f'new rule {rule}')
+        
+        # Strutture dati ottimizzate
+        self.rules: Dict[str, Rule] = {}
+        self._rule_hashes: Set[str] = set()
+        self._update_lock = Lock()
+        
+        if rules:
+            for rule in rules:
+                self.add_rule(rule)
 
     def get_rules(self):
-        return self.rules.values()
+        return list(self.rules.values())
 
     def delete_rule(self, rule_or_id):
-        if type(rule_or_id) is str and rule_or_id in self.rules:
-            del self.rules[rule_or_id]
-        elif type(rule_or_id) is Rule and rule_or_id.get_hash() in self.rules:
-            del self.rules[rule_or_id.get_hash()]
+        with self._update_lock:
+            if isinstance(rule_or_id, str):
+                self.rules.pop(rule_or_id, None)
+            elif isinstance(rule_or_id, Rule):
+                self.rules.pop(rule_or_id.get_hash(), None)
 
     def __sub__(self, rule_or_id):
         self.delete_rule(rule_or_id)
+        return self
 
     def get_rule(self, rule_id):
-        return self.rules.get(rule_id, None)
+        return self.rules.get(rule_id)
 
     def __getitem__(self, rule_id):
         return self.get_rule(rule_id)
 
     def add_rule(self, rule_str):
-        rule_obj = Rule(rule_str)
-        self.rules[rule_obj.get_hash()] = rule_obj
-        return rule_obj
+        with self._update_lock:
+            rule = Rule(rule_str)
+            self.rules[rule.get_hash()] = rule
+            return rule
 
     def __add__(self, rule_str):
         return self.add_rule(rule_str)
 
-    def check_for_new_rule(self):
-        actual_rules = set(self.main_shared_dict[FW_RULES_HASH_SET].keys())
-        actual_rules_len = len(actual_rules)
-        rules_len = len(self.rules)
-        if actual_rules_len == rules_len: # nessuna modifica
-            return
-        elif actual_rules_len > rules_len: # aggiunte
-            for added_rule in actual_rules-set(self.rules.keys()):
-                r = Rule(self.main_shared_dict[FW_RULES_HASH_SET][added_rule])
-                self.rules[added_rule] = r
-                self.main_logger.info(f'new rule {r.rule_str}')
-        else: # actual_rules_len < rules_len: # eliminate
-            for deleted_rule in set(self.rules.keys())-actual_rules:
-                del self.rules[deleted_rule]
-                self.main_logger.info(f'deleted rule with hash {deleted_rule}')
+    def _update_rules(self):
+        with self._update_lock:
+            current_hashes = set(self.main_shared_dict[FW_RULES_HASH_SET].keys())
+            
+            # Aggiunte
+            added = current_hashes - self._rule_hashes
+            for rule_hash in added:
+                rule_str = self.main_shared_dict[FW_RULES_HASH_SET][rule_hash]
+                self.rules[rule_hash] = Rule(rule_str)
+                if self.main_logger.isEnabledFor(logging.INFO):
+                    self.main_logger.info(f'New rule added: {rule_str[:50]}...')
+            
+            # Rimozioni
+            removed = self._rule_hashes - current_hashes
+            for rule_hash in removed:
+                self.rules.pop(rule_hash, None)
+                if self.main_logger.isEnabledFor(logging.INFO):
+                    self.main_logger.info(f'Rule deleted: {rule_hash}')
+            
+            self._rule_hashes = current_hashes
 
     def judge(self, packet, numpy_packet, tags=None):
-        self.check_for_new_rule()
-        result = all(rule.judge(packet, numpy_packet, tags) for rule in self.rules.values())
-        if result:
-            packet.accept()
-        else:
-            packet.drop()
-        return result
+        self._update_rules()
+        
+        try:
+            # Verifica tutte le regole in parallelo (se possibile)
+            result = all(rule.judge(packet, numpy_packet, tags) 
+                        for rule in self.rules.values())
+            
+            if result:
+                packet.accept()
+            else:
+                packet.drop()
+                
+            return result
+        except Exception as e:
+            self.main_logger.error(f"Error judging packet: {e}")
+            packet.accept()  # Fail-open per sicurezza
+            return True
 
-    def __call__(self, packet):
-        return self.judge(packet)
+    def __call__(self, packet, numpy_packet):
+        return self.judge(packet, numpy_packet)
