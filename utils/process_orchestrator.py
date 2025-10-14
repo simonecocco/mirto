62d1a5309e1b8f8aa93bb7607ad6ec710a1f8710
simonecocco <coccosimone@proton.me>
2025-10-14 20:49:29 +0200

    Removed property decorator and fixed new_process

--- utils/process_orchestrator.py
+++ utils/process_orchestrator.py
@@ -6,30 +6,27 @@ class ProcessOrchestrator:
     def __init__(self, process_synchronizer, user_prefs, logger):
         self._process_synchronizer = process_synchronizer
         self._processes = {}
-        self._shared_data = Manager.dict()
+        self._manager = Manager()
+        self._shared_data = self._manager.dict()
         self._logger = logger
         self._user_prefs = user_prefs
 
-    @property
     def get_user_prefs(self):
         return self._user_prefs
 
-    @property
     def get_logger(self):
         return self._logger
 
-    @property
     def get_process_synchronizer(self):
         return self._process_synchronizer
 
-    @property
     def get_shared_data(self):
         return self._shared_data
 
     def new_process(self, name, function):
         self._processes[name] = Process(
             target=function,
-            args=(self)
+            args=(self,)
         )
         self._processes[name].start()
 
