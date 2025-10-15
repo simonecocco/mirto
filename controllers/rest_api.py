88424e15b57474f51778077ba2b3510f0c9cd31b
simonecocco <coccosimone@proton.me>
2025-10-15 13:27:28 +0200

    Configured various routers

--- controllers/rest_api.py
+++ controllers/rest_api.py
@@ -1,22 +1,20 @@
-from json import dumps
-from os import urandom
 from flask import (
     Flask,
-    request,
     Response,
-    Blueprint,
     render_template
 )
 from flask_basicauth import BasicAuth
-from utils.const import *
-from utils.generic import add_iptables_rule
-from controllers.firewall.rule import Rule
 from user.user_preferences import (
     DEFAULT_MIRTO_PORT_KEY,
     DEFAULT_MIRTO_HOST_KEY,
     DEFAULT_MIRTO_PASSWORD_KEY,
     DEFAULT_MIRTO_USERNAME_KEY,
 )
+from controllers.routers.packet import PacketAPI
+from controllers.routers.packets import PacketListAPI
+from controllers.routers.rule import RuleAPI
+from controllers.routers.service import ServiceAPI
+from controllers.routers.services import ServicesAPI
 
 
 class MirtoAPI:
@@ -25,10 +23,10 @@ class MirtoAPI:
 
     def __init__(self, process_orchestrator):
         self._process_orchestrator = process_orchestrator
-        self._app = app = Flask(__name__)
+        self._app = Flask(__name__)
         self._configure_app()
         self._auth = BasicAuth(self._app)
-        self._register_blueprints()
+        self._routers = []
         self._add_routes()
 
     def _configure_app(self):
@@ -41,11 +39,23 @@ class MirtoAPI:
         self._app.config[MirtoAPI.APP_USERNAME_KEY] = mirto_username
         self._app.config[MirtoAPI.APP_PASSWORD_KEY] = mirto_password
 
-    def _register_blueprints(self):
-        #self._app._register_blueprints #TODO
-        pass
-
     def _add_routes(self):
+        def views_as_url_rule(apis):
+            for api in apis:
+                view_as_url_rule(api)
+
+        def view_as_url_rule(api):
+            view_func = api.as_view(
+                api.API_NAME,
+                self._auth,
+                self._process_orchestrator
+            )
+            self._app.add_url_rule(
+                api.BASE_PATH,
+                view_func=view_func,
+                methods=api.SUPPORTED_METHODS
+            )
+
         self.get_status = self._app.route(
             '/status', methods=['GET']
         )(self.get_status)
@@ -53,6 +63,14 @@ class MirtoAPI:
         self.home = self._app.route(
             '/home', methods=['GET']
         )(self._auth.required(self.home))
+
+        views_as_url_rule((
+            ServiceAPI,
+            ServicesAPI,
+            PacketListAPI,
+            PacketAPI,
+            RuleAPI
+        ))
 
     def run(self):
         user_pref = self._process_orchestrator.get_user_prefs()
@@ -64,10 +82,10 @@ class MirtoAPI:
 
     def get_status(self):
         return Response('OK', status=200)
-    
+
     def home(self):
         return render_template("index.html")
-    
+
 
 def start_rest_api(process_orchestrator):
     api_interface = MirtoAPI(process_orchestrator)
